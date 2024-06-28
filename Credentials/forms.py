d0a70eed60cbc7a43728ac0b64e59c7583c94199
hani-2020 <haniroshan2002@gmail.com>
2024-06-28 18:38:28 +0530

    birthday not being submitted

--- Credentials/forms.py
+++ Credentials/forms.py
@@ -4,10 +4,21 @@ from allauth.account.forms import SignupForm, LoginForm
 
 class CustomSignupForm(SignupForm):
     """Custom signup form which will have first name, last name and other details"""
+    def __init__(self, *args, **kwargs):
+        super(CustomSignupForm, self).__init__(*args, **kwargs)
+        del self.fields['password2']
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    birthday = forms.DateField()
+    birthday = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     email = forms.EmailField(max_length=254, required=True)
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.birthday = self.cleaned_data['birthday']
+        user.email = self.cleaned_data['email']
+        user.save()
+        return user
 
 class CustomLoginForm(LoginForm):
     """Custom login form to take in email and password from user"""
--- Credentials/views.py
+++ Credentials/views.py
@@ -1,4 +1,11 @@
 """Create your views here."""
 from django.shortcuts import render
-from allauth.account.views import SignupView
-from .forms import CustomSignupForm
+from allauth.account.views import SignupView, LoginView
+from .forms import CustomSignupForm, CustomLoginForm
+
+class CustomSignupView(SignupView):
+    """To make custom signup view"""
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CustomSignupForm()
+        return context
